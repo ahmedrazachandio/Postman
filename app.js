a170f7d75161a71d89e03353a1d29cdbfab44d4f
AhmadRaza <ahmadraza.azt@gmail.com>
2022-09-03 01:58:44 +0500

    if parameters box click than hide a Json box and show params box

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // Ahmed Raza 
 
 console.log("Post Master");
+// Hide the parameters box initially
+let parametersBox = document.getElementById('parametersBox');
+parametersBox.style.display = 'none';
+
+// If the user click on params hide the json box
+
+let paramsRadio = document.getElementById('paramsRadio');
+
+paramsRadio.addEventListener('click', ()=>{
+    document.getElementById('requestJsonBox').style.display = 'none';
+    document.getElementById('parametersBox').style.display = 'block';
+
+
+});
+
+
+// If the user click on json box hide the params box
