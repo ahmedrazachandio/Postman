4ae122971a44874fa86005d35a012c85971eba84
AhmadRaza <ahmadraza.azt@gmail.com>
2022-09-17 22:44:38 +0500

    add params button work

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Ahmed Raza 
 
+
+// Initialize no of parameters
+let addedParamsCount = 0;
+
 console.log("Post Master");
 // Hide the parameters box initially
 let parametersBox = document.getElementById('parametersBox');
@@ -29,10 +33,22 @@ jsonRadio.addEventListener('click', ()=>{
 
 
 
-let addParam = document.getElementById('addParams');
+let addParam = document.getElementById('addParam');
 
 addParam.addEventListener('click', ()=>{
-    let param =document.getElementById('params');
+    let params =document.getElementById('params');
+    let string = ` <div class="form row">
+                        <label for="url" class="form-label col-sm-2 col-form-label">Parameter ${addedParamsCount +2}</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="parameterKey1" placeholder="Enter Parameter ${addedParamsCount +2} Key">
+                        </div>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="parameterValue${addedParamsCount +2}" placeholder="Enter Parameter ${addedParamsCount +2} Value">
+                        </div>
+                        <button class="btn btn-primary col-sm-1" id="addParam">-</button>
+                    </div>`;
+    addedParamsCount ++;
+    
 
 
 });
